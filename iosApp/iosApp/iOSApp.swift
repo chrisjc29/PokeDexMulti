64cd51d85eb2e13606a510c82d4ca40f1554d819
chris curran <chris.curran@tribusdigital.com>
2023-11-12 20:26:08 +0000

    Version update plus, swift changes

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,11 +1,19 @@
 import SwiftUI
 
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 @main
 struct iOSApp: App {
     
-    init(){
-        FirebaseApp.configure()
-      }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
 	var body: some Scene {
 		WindowGroup {
